c40c809100a79fa57b1d6c57124955bbf7afe36b
DANIEL GUILLERMO <DANIEL GUILLERMO HERNANDEZ SOTELO>
2018-11-09 15:01:15 -0500

    add inert plugin and  path for static files

--- index.js
+++ index.js
@@ -1,35 +1,47 @@
 'use strict'
 
 const Hapi = require('hapi')
+const inert = require('inert')
+const path = require('path')
 
 const server = Hapi.server({
 	port: process.env.PORT || 4000,
-	host: 'localhost'
+	host: 'localhost',
+	routes: {
+		files: {
+			relativeTo: path.join(__dirname, 'public') // ruta relativa para todas las rutas de la app - __dirname significa el dir actual
+		}
+	}
 })
 
 async function init() {
-	server.route({
-		method: 'GET',
-		path: '/',
-		handler: (req, h) => {
-			return h.response('Hola mundo...').code(200)
-		}
-	})
-
-	server.route({
-		method: 'GET',
-		path: '/redirect',
-		handler: (req, h) => {
-			return h.redirect('http://platzi.com')
-		}
-	})
-
 	try {
+		await server.register(inert)
 		await server.start()
 	} catch (error) {
 		console.error(error) /* controlando error */
 		process.exit(1) /* terminando proceso */
 	}
+
+	server.route({
+		method: 'GET',
+		path: '/home',
+		handler: (req, h) => {
+			// return h.response('Hola mundo...').code(200)
+			return h.file('index.html')
+		}
+	})
+
+	server.route({
+		method: 'GET',
+		path: '/{param*}',
+		handler: {
+			directory: {
+				path: '.',
+				index: ['index.html']
+			}
+		}
+	})
 
 	console.log(`Servidor lanzado en: ${server.info.uri}`)
 }
